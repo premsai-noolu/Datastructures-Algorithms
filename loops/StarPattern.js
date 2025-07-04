7ea702e2ec0888f2a011dc761550956a4e1197f9
premsai-noolu <premsainoolu01@gmail.com>
2025-07-04 10:14:54 +0530

    Star pattern logics

--- loops/StarPattern.js
+++ loops/StarPattern.js
@@ -87,3 +87,43 @@ for (let i = 0; i < n; i++) {
   }
   console.log(row);
 }
+
+// 1
+// 10
+// 101
+// 1010
+// 10101
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  let toggle = 1;
+  for (let j = 0; j < i + 1; j++) {
+    row = row + toggle;
+    if (toggle == 1) {
+      toggle = 0;
+    } else {
+      toggle = 1;
+    }
+  }
+  console.log(row);
+}
+
+// 1
+// 01
+// 010
+// 1010
+// 10101
+
+let toggling = 1;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < i + 1; j++) {
+    row = row + toggling;
+    if (toggling == 1) {
+      toggling = 0;
+    } else {
+      toggling = 1;
+    }
+  }
+  console.log(row);
+}
