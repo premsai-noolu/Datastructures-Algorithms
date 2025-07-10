256ee34eb87ff5aea3124e0c37f7bb7dda1ffd5a
premsai-noolu <premsainoolu01@gmail.com>
2025-07-10 19:24:35 +0530

    sum of odd numbers in an array

---
+++ Recursion/sSumOfOddNumbersInArray.js
@@ -0,0 +1,10 @@
+let arr = [5, 3, 2, 10, 1];
+
+function sum(n) {
+  let isOdd = arr[n] % 2 != 0;
+  if (n == 0) {
+    return isOdd ? arr[n] : 0;
+  }
+  return (isOdd ? arr[n] : 0) + sum(n - 1);
+}
+console.log(sum(arr.length - 1));
