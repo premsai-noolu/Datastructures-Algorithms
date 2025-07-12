32970555adcc65b25aacae6cfcd4980f09a034a5
premsai-noolu <premsainoolu01@gmail.com>
2025-07-12 15:19:16 +0530

    bubble sort

---
+++ Searching and Sorting/BubbleSort.js
@@ -0,0 +1,20 @@
+let arr = [4, 5, 1, 3, 9];
+
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let isSwapped = false;
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        isSwapped = true;
+      }
+    }
+    if (!isSwapped) break;
+  }
+  return arr;
+}
+let result = bubbleSort(arr);
+console.log("Sorted array", result);
