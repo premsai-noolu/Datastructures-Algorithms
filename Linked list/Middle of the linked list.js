880891361ae962d10c29fd6b968e327871386a80
premsai-noolu <premsainoolu01@gmail.com>
2025-07-16 22:04:31 +0530

    Middle of the linked list

---
+++ Linked list/Middle of the linked list.js
@@ -0,0 +1,11 @@
+//876. Middle of the Linked List
+
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
