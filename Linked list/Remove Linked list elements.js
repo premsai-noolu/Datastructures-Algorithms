8ed84e77b0851be135e6a094511e32586d22efdc
premsai-noolu <premsainoolu01@gmail.com>
2025-07-18 10:05:23 +0530

    Remove linked list elements

---
+++ Linked list/Remove Linked list elements.js
@@ -0,0 +1,16 @@
+//203. Remove Linked List Elements
+
+var removeElements = function (head, val) {
+  let sentinel = new ListNode();
+  sentinel.next = head;
+
+  let prev = sentinel;
+  while (prev && prev.next) {
+    if (prev.next.val === val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+  return sentinel.next;
+};
