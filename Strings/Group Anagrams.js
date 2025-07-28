f0933aeab2b8eff22b457ac191b265d77b67efbd
premsai-noolu <premsainoolu01@gmail.com>
2025-07-28 20:16:38 +0530

    Group anagrams

--- Strings/Group Anagrams.js
+++ Strings/Group Anagrams.js
@@ -18,3 +18,29 @@ var groupAnagrams = function (strs) {
   }
   return ans;
 };
+
+//Best approach
+
+var groupAnagrams = function (strs) {
+  let map = {};
+  for (let i = 0; i < strs.length; i++) {
+    let s = strs[i];
+    let freq = Array(26).fill(0);
+    for (let j = 0; j < s.length; j++) {
+      let index = s[j].charCodeAt() - "a".charCodeAt();
+      ++freq[index];
+    }
+
+    let key = "";
+    for (let k = 0; k < 26; k++) {
+      key = key + "#" + freq[k];
+    }
+
+    if (!map[key]) {
+      map[key] = [s];
+    } else {
+      map[key].push(s);
+    }
+  }
+  return [...Object.values(map)];
+};
