2852ffb1c4b1d6d6cf09469281ea4656b7f5a999
premsai-noolu <premsainoolu01@gmail.com>
2025-08-03 09:58:08 +0530

    remove outermost parenthesis without using stack

--- Stacks and Queues/Remove Outermpst parenthesis.js
+++ Stacks and Queues/Remove Outermpst parenthesis.js
@@ -19,3 +19,20 @@ var removeOuterParentheses = function (s) {
   }
   return ans;
 };
+
+// without using stack(Good approach)
+
+var removeOuterParentheses = function (s) {
+  let level = 0;
+  let ans = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      ++level;
+      ans = ans + (level > 1 ? s[i] : "");
+    } else {
+      ans = ans + (level > 1 ? s[i] : "");
+      --level;
+    }
+  }
+  return ans;
+};
