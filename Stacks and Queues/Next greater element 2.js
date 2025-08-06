e1fc9888805689daa70aba078bdd11bcf37f7df5
premsai-noolu <premsainoolu01@gmail.com>
2025-08-06 22:16:23 +0530

    next greater element-2

--- Stacks and Queues/Next greater element 2.js
+++ Stacks and Queues/Next greater element 2.js
@@ -1,5 +1,6 @@
 //503. Next Greater Element II
 
+//first approach
 var nextGreaterElements = function (nums) {
   let arr = [...nums, ...nums];
 
@@ -24,4 +25,28 @@ var nextGreaterElements = function (nums) {
     stack.push(arr[i]);
   }
   return ans.slice(0, nums.length);
+};
+
+//second approach
+
+var nextGreaterElements = function (nums) {
+  let stack = [];
+
+  let n = nums.length;
+  let arr = Array(n).fill(-1);
+  stack.push(nums[n - 1]);
+  arr[n - 1] = -1;
+  for (let i = 2 * n - 2; i >= 0; i--) {
+    while (stack.length) {
+      let top = stack[stack.length - 1];
+      if (nums[i % n] < top) {
+        arr[i % n] = top;
+        break;
+      } else {
+        stack.pop();
+      }
+    }
+    stack.push(nums[i % n]);
+  }
+  return arr;
 };
