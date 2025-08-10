f97e3a93866fed0e6c0851f9da004803a67e05b6
premsai-noolu <premsainoolu01@gmail.com>
2025-08-10 12:14:21 +0530

    Search in a rotated sorted array

---
+++ Binary Search Algorithm/Search in a rotated sorted array.js
@@ -0,0 +1,27 @@
+//33. Search in Rotated Sorted Array
+
+var search = function (nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2);
+
+    if (nums[m] === target) {
+      return m;
+    } else if (nums[l] <= nums[m]) {
+      if (target >= nums[l] && target < nums[m]) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
+    } else {
+      if (target > nums[m] && target <= nums[r]) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+  }
+  return -1;
+};
