55c68977c71f35b5c2a015e7246fd3a67ecd168d
premsai-noolu <premsainoolu01@gmail.com>
2025-08-13 15:54:06 +0530

    K closest elements

---
+++ Binary Search Algorithm/Find K closest elements.js
@@ -0,0 +1,27 @@
+//658. Find K Closest Elements
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let l = 0;
+  let r = arr.length - k;
+
+  while (l < r) {
+    let m = Math.floor((l + r) / 2);
+    if (arr[m + k] - x < x - arr[m]) {
+      l = m + 1;
+    } else {
+      r = m;
+    }
+  }
+
+  let ans = [];
+  for (let i = l; i < l + k; i++) {
+    ans.push(arr[i]);
+  }
+  return ans;
+};
