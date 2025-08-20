e12cf015724fa7b3de2b1864506e249d039b7902
premsai-noolu <premsainoolu01@gmail.com>
2025-08-20 10:46:10 +0530

    Postorder iterative approach(2 stacks)

--- Binary Tree/Postorder traversal.js
+++ Binary Tree/Postorder traversal.js
@@ -10,4 +10,24 @@ var postorderTraversal = function (root) {
   }
   traversal(root);
   return ans;
+
+  //Iterative approach using 2 stacks
+
+  let s1 = [];
+  let s2 = [];
+  if (!root) return [];
+  s1.push(root);
+  while (s1.length) {
+    curr = s1.pop();
+    s2.push(curr);
+    curr.left && s1.push(curr.left);
+    curr.right && s1.push(curr.right);
+  }
+
+  let ans = [];
+  while (s2.length) {
+    ans.push(s2.pop().val);
+  }
+
+  return ans;
 };
