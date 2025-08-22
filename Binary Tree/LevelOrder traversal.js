321bb55ea4e7c73cfbd630c4adffa2b3016d0280
premsai-noolu <premsainoolu01@gmail.com>
2025-08-22 10:24:06 +0530

    Binary tree Levelorder traversal.js

---
+++ Binary Tree/LevelOrder traversal.js
@@ -0,0 +1,21 @@
+//102. Binary Tree Level Order Traversal
+
+var levelOrder = function (root) {
+  if (!root) return [];
+  let curr = root;
+  let q = [root];
+  let ans = [];
+  while (q.length) {
+    let levelArr = [];
+    let levelSize = q.length;
+    for (let i = 0; i < levelSize; i++) {
+      curr = q.shift();
+      curr.left && q.push(curr.left);
+      curr.right && q.push(curr.right);
+      levelArr.push(curr.val);
+    }
+    ans.push(levelArr);
+    console.log(ans);
+  }
+  return ans;
+};
