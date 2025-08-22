f32e050628b5862fff5a118c3c672227484f7a33
premsai-noolu <premsainoolu01@gmail.com>
2025-08-22 11:00:47 +0530

    level order traversal recursive solution

--- Binary Tree/LevelOrder traversal.js
+++ Binary Tree/LevelOrder traversal.js
@@ -18,4 +18,16 @@ var levelOrder = function (root) {
     console.log(ans);
   }
   return ans;
+
+  //Recursive Solution
+  if (!root) return [];
+  let ans = [];
+  function traversal(curr, level) {
+    if (!ans[level]) ans[level] = [];
+    ans[level].push(curr.val);
+    curr.left && traversal(curr.left, level + 1);
+    curr.right && traversal(curr.right, level + 1);
+  }
+  traversal(root, 0);
+  return ans;
 };
