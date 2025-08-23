eea54136f35dd92ae56c8d9c18bccf9c4c1e4615
premsai-noolu <premsainoolu01@gmail.com>
2025-08-23 15:58:40 +0530

    Invert a binary tree

---
+++ Binary Tree/Invert binary tree.js
@@ -0,0 +1,14 @@
+//226. Invert Binary Tree
+
+var invertTree = function (root) {
+  if (!root) return root;
+
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
