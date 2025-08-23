0075013dec9a3a520612c02c16c4757dca9a7655
premsai-noolu <premsainoolu01@gmail.com>
2025-08-23 16:07:16 +0530

    Same tree

---
+++ Binary Tree/Same tree.js
@@ -0,0 +1,9 @@
+//100. Same Tree
+
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  return (
+    p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  );
+};
