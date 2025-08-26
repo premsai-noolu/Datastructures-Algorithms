4aeb9eea63019dd67446e09ba78da51079d91558
premsai-noolu <premsainoolu01@gmail.com>
2025-08-26 14:39:41 +0530

    lca of bst

---
+++ Binary Search Tree/LCA of BST.js
@@ -0,0 +1,11 @@
+//235. Lowest Common Ancestor of a Binary Search
+
+var lowestCommonAncestor = function (root, p, q) {
+  if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+};
