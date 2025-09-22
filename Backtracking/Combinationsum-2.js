46872f7ff2a7ed60ada413c374479e82cb1dfdff
premsai-noolu <premsainoolu01@gmail.com>
2025-09-22 20:09:32 +0530

    combinations

--- Backtracking/Combinationsum-2.js
+++ Backtracking/Combinationsum-2.js
@@ -1,18 +1,18 @@
-40. Combination Sum II
+//40. Combination Sum II
 
-var combinationSum2 = function(arr, target) {
-    let result=[]
-    arr.sort((a,b) => (a-b))
-    let backtrack=(remainingSum,path,start) => {
-        if(remainingSum === 0) result.push([...path])
-        if(remainingSum<0) return
-        for(let i=start;i<arr.length;i++){
-            if(i>start && arr[i-1] === arr[i]) continue
-            path.push(arr[i])
-            backtrack(remainingSum-arr[i],path,i+1)
-            path.pop()
-        }
+var combinationSum2 = function (arr, target) {
+  let result = [];
+  arr.sort((a, b) => a - b);
+  let backtrack = (remainingSum, path, start) => {
+    if (remainingSum === 0) result.push([...path]);
+    if (remainingSum < 0) return;
+    for (let i = start; i < arr.length; i++) {
+      if (i > start && arr[i - 1] === arr[i]) continue;
+      path.push(arr[i]);
+      backtrack(remainingSum - arr[i], path, i + 1);
+      path.pop();
     }
-    backtrack(target,[],0)
-    return result
+  };
+  backtrack(target, [], 0);
+  return result;
 };
