1d5e516a110c72f856448bb3cfc5cd0c6776eae2
premsai-noolu <premsainoolu01@gmail.com>
2025-09-24 22:04:38 +0530

    letter combinations of a phone number

---
+++ Backtracking/Letter combinations of a phone number.js
@@ -0,0 +1,30 @@
+//17. Letter Combinations of a Phone Number
+
+var letterCombinations = function (digits) {
+  if (!digits.length) return [];
+  let result = [];
+  let letters = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  let backtrack = (path, index) => {
+    if (index == digits.length) {
+      result.push(path.join(""));
+      return;
+    }
+    let choices = letters[digits[index]];
+    for (let i = 0; i < choices.length; i++) {
+      path.push(choices[i]);
+      backtrack(path, index + 1);
+      path.pop();
+    }
+  };
+  backtrack([], 0);
+  return result;
+};
