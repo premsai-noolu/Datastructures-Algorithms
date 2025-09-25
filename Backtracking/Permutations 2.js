f5b4c0f23aafa1f29b5d9ccadf49b84aec41d852
premsai-noolu <premsainoolu01@gmail.com>
2025-09-25 14:52:10 +0530

    permutations-2

---
+++ Backtracking/Permutations 2.js
@@ -0,0 +1,21 @@
+//47. Permutations II
+
+var permuteUnique = function (arr) {
+  let result = [];
+  arr.sort((a, b) => a - b);
+
+  let backtrack = (path, choices) => {
+    if (path.length == arr.length) {
+      result.push([...path]);
+      return;
+    }
+    for (let i = 0; i < choices.length; i++) {
+      if (i > 0 && choices[i] === choices[i - 1]) continue;
+      path.push(choices[i]);
+      backtrack(path, [...choices.slice(0, i), ...choices.slice(i + 1)]);
+      path.pop();
+    }
+  };
+  backtrack([], arr);
+  return result;
+};
