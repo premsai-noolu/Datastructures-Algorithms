ae1ec293576ace2c50df1aa90ab1201fb7d0fea2
premsai-noolu <premsainoolu01@gmail.com>
2025-10-12 16:33:20 +0530

    Min cost climbingStairs

---
+++ Dynamic Programming/Min cost climbingStairs.js
@@ -0,0 +1,9 @@
+//746. Min Cost Climbing Stairs
+
+var minCostClimbingStairs = function (cost) {
+  let dp = [0, 0];
+  for (let i = 2; i <= cost.length; i++) {
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+  }
+  return dp[cost.length];
+};
